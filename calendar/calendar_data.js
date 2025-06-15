5bea678f28a8099a87b06c6ee957368ba53caa43
Luca Canepa <121318671+Lucanepa@users.noreply.github.com>
2025-06-15 12:09:05 +0200

    Add files via upload

---
+++ calendar/calendar_data.js
@@ -0,0 +1,740 @@
+const calendarEvents = [
+  {
+    "title": "KSC Wiedikon H1 - VBC Wetzikon H1",
+    "start": "2025-09-27T13:30:00",
+    "className": [
+      "open-event",
+      "spielsamstag"
+    ],
+    "extendedProps": {
+      "group": "Herren 2. Liga",
+      "gym": "Kantonsschule Wiedikon C",
+      "status": "Open",
+      "homeaway": "Home",
+      "wiedikonTeam": "KSC Wiedikon H1"
+    }
+  },
+  {
+    "title": "KSC Wiedikon D2 - VBC Wetzikon D1",
+    "start": "2025-09-27T13:30:00",
+    "className": [
+      "open-event",
+      "spielsamstag"
+    ],
+    "extendedProps": {
+      "group": "Frauen 3. Liga Gruppe B",
+      "gym": "Kantonsschule Wiedikon (Halle B)",
+      "status": "Open",
+      "homeaway": "Home",
+      "wiedikonTeam": "KSC Wiedikon D2"
+    }
+  },
+  {
+    "title": "KSC Wiedikon H2 - VBC Spada Academica H2",
+    "start": "2025-10-22T20:00:00",
+    "className": [
+      "open-event"
+    ],
+    "extendedProps": {
+      "group": "Herren 3. Liga Gruppe A",
+      "gym": "Kantonsschule Wiedikon (Halle B)",
+      "status": "Open",
+      "homeaway": "Home",
+      "wiedikonTeam": "KSC Wiedikon H2"
+    }
+  },
+  {
+    "title": "KSC Wiedikon H2 - VBC Züri Unterland H3",
+    "start": "2025-11-26T20:00:00",
+    "className": [
+      "open-event"
+    ],
+    "extendedProps": {
+      "group": "Herren 3. Liga Gruppe A",
+      "gym": "Kantonsschule Wiedikon (Halle A)",
+      "status": "Open",
+      "homeaway": "Home",
+      "wiedikonTeam": "KSC Wiedikon H2"
+    }
+  },
+  {
+    "title": "KSC Wiedikon H2 - Rainbow Sport Zürich",
+    "start": "2026-01-31T16:00:00",
+    "className": [
+      "open-event",
+      "spielsamstag"
+    ],
+    "extendedProps": {
+      "group": "Herren 3. Liga Gruppe A",
+      "gym": "Kantonsschule Wiedikon (Halle B)",
+      "status": "Open",
+      "homeaway": "Home",
+      "wiedikonTeam": "KSC Wiedikon H2"
+    }
+  },
+  {
+    "title": "KSC Wiedikon H3 - Volley Uster H2",
+    "start": "2025-09-23T20:30:00",
+    "className": [
+      "confirmed-event"
+    ],
+    "extendedProps": {
+      "group": "Herren 3. Liga Gruppe B",
+      "gym": "Krämeracker 1-2",
+      "status": "Confirmed",
+      "homeaway": "Away",
+      "wiedikonTeam": "KSC Wiedikon H3"
+    }
+  },
+  {
+    "title": "KSC Wiedikon H3 - VBC Wetzikon H2",
+    "start": "2025-09-26T20:00:00",
+    "className": [
+      "open-event"
+    ],
+    "extendedProps": {
+      "group": "Herren 3. Liga Gruppe B",
+      "gym": "Kantonsschule Wiedikon (Halle A)",
+      "status": "Open",
+      "homeaway": "Home",
+      "wiedikonTeam": "KSC Wiedikon H3"
+    }
+  },
+  {
+    "title": "KSC Wiedikon H3 - VC Tornado Adliswil H1",
+    "start": "2025-11-12T20:00:00",
+    "className": [
+      "open-event"
+    ],
+    "extendedProps": {
+      "group": "Herren 3. Liga Gruppe B",
+      "gym": "Kantonsschule Wiedikon (Halle A)",
+      "status": "Open",
+      "homeaway": "Home",
+      "wiedikonTeam": "KSC Wiedikon H3"
+    }
+  },
+  {
+    "title": "KSC Wiedikon H3 - VBC Volewa Wald H1",
+    "start": "2025-11-27T21:00:00",
+    "className": [
+      "open-event"
+    ],
+    "extendedProps": {
+      "group": "Herren 3. Liga Gruppe B",
+      "gym": "Sporthalle Elba",
+      "status": "Open",
+      "homeaway": "Away",
+      "wiedikonTeam": "KSC Wiedikon H3"
+    }
+  },
+  {
+    "title": "KSC Wiedikon H3 - Volley Oerlikon H2",
+    "start": "2025-12-10T20:00:00",
+    "className": [
+      "open-event"
+    ],
+    "extendedProps": {
+      "group": "Herren 3. Liga Gruppe B",
+      "gym": "Kantonsschule Wiedikon (Halle A)",
+      "status": "Open",
+      "homeaway": "Home",
+      "wiedikonTeam": "KSC Wiedikon H3"
+    }
+  },
+  {
+    "title": "KSC Wiedikon H3 - VBC Rämi H2",
+    "start": "2026-01-09T20:00:00",
+    "className": [
+      "open-event"
+    ],
+    "extendedProps": {
+      "group": "Herren 3. Liga Gruppe B",
+      "gym": "Kantonsschule Wiedikon (Halle A)",
+      "status": "Open",
+      "homeaway": "Home",
+      "wiedikonTeam": "KSC Wiedikon H3"
+    }
+  },
+  {
+    "title": "KSC Wiedikon H3 - VBC Volewa Wald H1",
+    "start": "2026-01-30T20:00:00",
+    "className": [
+      "open-event"
+    ],
+    "extendedProps": {
+      "group": "Herren 3. Liga Gruppe B",
+      "gym": "Kantonsschule Wiedikon (Halle A)",
+      "status": "Open",
+      "homeaway": "Home",
+      "wiedikonTeam": "KSC Wiedikon H3"
+    }
+  },
+  {
+    "title": "KSC Wiedikon H3 - Volley Uster H2",
+    "start": "2026-02-06T20:00:00",
+    "className": [
+      "open-event"
+    ],
+    "extendedProps": {
+      "group": "Herren 3. Liga Gruppe B",
+      "gym": "Kantonsschule Wiedikon (Halle A)",
+      "status": "Open",
+      "homeaway": "Home",
+      "wiedikonTeam": "KSC Wiedikon H3"
+    }
+  },
+  {
+    "title": "KSC Wiedikon D3 - VBC Limmattal D3",
+    "start": "2025-11-14T20:00:00",
+    "className": [
+      "open-event"
+    ],
+    "extendedProps": {
+      "group": "Frauen 5. Liga Gruppe A",
+      "gym": "Kantonsschule Wiedikon (Halle A)",
+      "status": "Open",
+      "homeaway": "Home",
+      "wiedikonTeam": "KSC Wiedikon D3"
+    }
+  },
+  {
+    "title": "KSC Wiedikon D3 - Wädivolley D4",
+    "start": "2026-01-31T13:30:00",
+    "className": [
+      "open-event",
+      "spielsamstag"
+    ],
+    "extendedProps": {
+      "group": "Frauen 5. Liga Gruppe A",
+      "gym": "Kantonsschule Wiedikon (Halle B)",
+      "status": "Open",
+      "homeaway": "Home",
+      "wiedikonTeam": "KSC Wiedikon D3"
+    }
+  },
+  {
+    "title": "KSC Wiedikon D3 - Duebendorf D1",
+    "start": "2026-03-07T16:00:00",
+    "className": [
+      "open-event",
+      "spielsamstag"
+    ],
+    "extendedProps": {
+      "group": "Frauen 5. Liga Gruppe A",
+      "gym": "Kantonsschule Wiedikon (Halle B)",
+      "status": "Open",
+      "homeaway": "Home",
+      "wiedikonTeam": "KSC Wiedikon D3"
+    }
+  },
+  {
+    "title": "KSC Wiedikon D4 - VBC Wetzikon D4",
+    "start": "2025-09-27T18:30:00",
+    "className": [
+      "open-event",
+      "spielsamstag"
+    ],
+    "extendedProps": {
+      "group": "Frauen 5. Liga Gruppe B",
+      "gym": "Kantonsschule Wiedikon (Halle A)",
+      "status": "Open",
+      "homeaway": "Home",
+      "wiedikonTeam": "KSC Wiedikon D4"
+    }
+  },
+  {
+    "title": "KSC Wiedikon DU23-2 - Volley S9 2",
+    "start": "2025-09-15T00:00:00",
+    "className": [
+      "open-event"
+    ],
+    "extendedProps": {
+      "group": "Frauen U23 2. Liga",
+      "gym": NaN,
+      "status": "Open",
+      "homeaway": "Away",
+      "wiedikonTeam": "KSC Wiedikon DU23-2"
+    }
+  },
+  {
+    "title": "KSC Wiedikon DU23-2 - Volley S9 2",
+    "start": "2025-09-15T00:00:00",
+    "className": [
+      "open-event"
+    ],
+    "extendedProps": {
+      "group": "Frauen U23 2. Liga",
+      "gym": NaN,
+      "status": "Open",
+      "homeaway": "Away",
+      "wiedikonTeam": "KSC Wiedikon DU23-2"
+    }
+  },
+  {
+    "title": "KSC Wiedikon DU23-2 - Rüschlikon U23",
+    "start": "2025-09-15T00:00:00",
+    "className": [
+      "open-event"
+    ],
+    "extendedProps": {
+      "group": "Frauen U23 2. Liga",
+      "gym": "Gulliver 1 und 2",
+      "status": "Open",
+      "homeaway": "Away",
+      "wiedikonTeam": "KSC Wiedikon DU23-2"
+    }
+  },
+  {
+    "title": "KSC Wiedikon DU23-2 - Rüschlikon U23",
+    "start": "2025-09-15T00:00:00",
+    "className": [
+      "open-event"
+    ],
+    "extendedProps": {
+      "group": "Frauen U23 2. Liga",
+      "gym": NaN,
+      "status": "Open",
+      "homeaway": "Home",
+      "wiedikonTeam": "KSC Wiedikon DU23-2"
+    }
+  },
+  {
+    "title": "KSC Wiedikon DU23-2 - VBC Limmattal",
+    "start": "2025-09-15T00:00:00",
+    "className": [
+      "open-event"
+    ],
+    "extendedProps": {
+      "group": "Frauen U23 2. Liga",
+      "gym": NaN,
+      "status": "Open",
+      "homeaway": "Home",
+      "wiedikonTeam": "KSC Wiedikon DU23-2"
+    }
+  },
+  {
+    "title": "KSC Wiedikon DU23-2 - VBC Limmattal",
+    "start": "2025-09-15T00:00:00",
+    "className": [
+      "open-event"
+    ],
+    "extendedProps": {
+      "group": "Frauen U23 2. Liga",
+      "gym": NaN,
+      "status": "Open",
+      "homeaway": "Away",
+      "wiedikonTeam": "KSC Wiedikon DU23-2"
+    }
+  },
+  {
+    "title": "KSC Wiedikon DU23-2 - Volley Obfelden U23",
+    "start": "2025-09-15T00:00:00",
+    "className": [
+      "open-event"
+    ],
+    "extendedProps": {
+      "group": "Frauen U23 2. Liga",
+      "gym": NaN,
+      "status": "Open",
+      "homeaway": "Home",
+      "wiedikonTeam": "KSC Wiedikon DU23-2"
+    }
+  },
+  {
+    "title": "KSC Wiedikon DU23-2 - Volley S9 2",
+    "start": "2025-09-15T00:00:00",
+    "className": [
+      "open-event"
+    ],
+    "extendedProps": {
+      "group": "Frauen U23 2. Liga",
+      "gym": NaN,
+      "status": "Open",
+      "homeaway": "Home",
+      "wiedikonTeam": "KSC Wiedikon DU23-2"
+    }
+  },
+  {
+    "title": "KSC Wiedikon DU23-2 - Rüschlikon U23",
+    "start": "2025-09-15T00:00:00",
+    "className": [
+      "open-event"
+    ],
+    "extendedProps": {
+      "group": "Frauen U23 2. Liga",
+      "gym": NaN,
+      "status": "Open",
+      "homeaway": "Home",
+      "wiedikonTeam": "KSC Wiedikon DU23-2"
+    }
+  },
+  {
+    "title": "KSC Wiedikon DU23-2 - Volley Obfelden U23",
+    "start": "2025-09-15T00:00:00",
+    "className": [
+      "open-event"
+    ],
+    "extendedProps": {
+      "group": "Frauen U23 2. Liga",
+      "gym": "Schlossächer",
+      "status": "Open",
+      "homeaway": "Away",
+      "wiedikonTeam": "KSC Wiedikon DU23-2"
+    }
+  },
+  {
+    "title": "KSC Wiedikon DU23-2 - VBC Rämi 1",
+    "start": "2025-09-15T00:00:00",
+    "className": [
+      "open-event"
+    ],
+    "extendedProps": {
+      "group": "Frauen U23 2. Liga",
+      "gym": NaN,
+      "status": "Open",
+      "homeaway": "Home",
+      "wiedikonTeam": "KSC Wiedikon DU23-2"
+    }
+  },
+  {
+    "title": "KSC Wiedikon DU23-2 - VBC Rämi 1",
+    "start": "2025-09-15T00:00:00",
+    "className": [
+      "open-event"
+    ],
+    "extendedProps": {
+      "group": "Frauen U23 2. Liga",
+      "gym": NaN,
+      "status": "Open",
+      "homeaway": "Away",
+      "wiedikonTeam": "KSC Wiedikon DU23-2"
+    }
+  },
+  {
+    "title": "KSC Wiedikon DU23-2 - VBC Rämi 1",
+    "start": "2025-09-15T00:00:00",
+    "className": [
+      "open-event"
+    ],
+    "extendedProps": {
+      "group": "Frauen U23 2. Liga",
+      "gym": NaN,
+      "status": "Open",
+      "homeaway": "Away",
+      "wiedikonTeam": "KSC Wiedikon DU23-2"
+    }
+  },
+  {
+    "title": "KSC Wiedikon DU23-2 - Volley Obfelden U23",
+    "start": "2025-09-15T00:00:00",
+    "className": [
+      "open-event"
+    ],
+    "extendedProps": {
+      "group": "Frauen U23 2. Liga",
+      "gym": NaN,
+      "status": "Open",
+      "homeaway": "Home",
+      "wiedikonTeam": "KSC Wiedikon DU23-2"
+    }
+  },
+  {
+    "title": "KSC Wiedikon DU23-2 - VBC Limmattal",
+    "start": "2025-09-15T00:00:00",
+    "className": [
+      "open-event"
+    ],
+    "extendedProps": {
+      "group": "Frauen U23 2. Liga",
+      "gym": NaN,
+      "status": "Open",
+      "homeaway": "Away",
+      "wiedikonTeam": "KSC Wiedikon DU23-2"
+    }
+  },
+  {
+    "title": "KSC Wiedikon DU23-1 - VBC Züri Unterland DU23-1",
+    "start": "2025-09-15T00:00:00",
+    "className": [
+      "open-event"
+    ],
+    "extendedProps": {
+      "group": "Frauen U23 1. Liga",
+      "gym": NaN,
+      "status": "Open",
+      "homeaway": "Home",
+      "wiedikonTeam": "KSC Wiedikon DU23-1"
+    }
+  },
+  {
+    "title": "KSC Wiedikon DU23-1 - Volleyball Academy",
+    "start": "2025-09-15T00:00:00",
+    "className": [
+      "open-event"
+    ],
+    "extendedProps": {
+      "group": "Frauen U23 1. Liga",
+      "gym": NaN,
+      "status": "Open",
+      "homeaway": "Home",
+      "wiedikonTeam": "KSC Wiedikon DU23-1"
+    }
+  },
+  {
+    "title": "KSC Wiedikon DU23-1 - VBC Furttal",
+    "start": "2025-09-15T00:00:00",
+    "className": [
+      "open-event"
+    ],
+    "extendedProps": {
+      "group": "Frauen U23 1. Liga",
+      "gym": "Zwingert",
+      "status": "Open",
+      "homeaway": "Away",
+      "wiedikonTeam": "KSC Wiedikon DU23-1"
+    }
+  },
+  {
+    "title": "KSC Wiedikon DU23-1 - VBC Einsiedeln DU23",
+    "start": "2025-09-15T00:00:00",
+    "className": [
+      "open-event"
+    ],
+    "extendedProps": {
+      "group": "Frauen U23 1. Liga",
+      "gym": NaN,
+      "status": "Open",
+      "homeaway": "Home",
+      "wiedikonTeam": "KSC Wiedikon DU23-1"
+    }
+  },
+  {
+    "title": "KSC Wiedikon DU23-1 - Volley Uster A1",
+    "start": "2025-09-15T00:00:00",
+    "className": [
+      "open-event"
+    ],
+    "extendedProps": {
+      "group": "Frauen U23 1. Liga",
+      "gym": "Krämeracker 1-2",
+      "status": "Open",
+      "homeaway": "Away",
+      "wiedikonTeam": "KSC Wiedikon DU23-1"
+    }
+  },
+  {
+    "title": "KSC Wiedikon DU23-1 - VBC Voléro Zürich 1",
+    "start": "2025-09-15T00:00:00",
+    "className": [
+      "open-event"
+    ],
+    "extendedProps": {
+      "group": "Frauen U23 1. Liga",
+      "gym": NaN,
+      "status": "Open",
+      "homeaway": "Home",
+      "wiedikonTeam": "KSC Wiedikon DU23-1"
+    }
+  },
+  {
+    "title": "KSC Wiedikon DU23-1 - Volleyball Academy",
+    "start": "2025-09-15T00:00:00",
+    "className": [
+      "open-event"
+    ],
+    "extendedProps": {
+      "group": "Frauen U23 1. Liga",
+      "gym": NaN,
+      "status": "Open",
+      "homeaway": "Away",
+      "wiedikonTeam": "KSC Wiedikon DU23-1"
+    }
+  },
+  {
+    "title": "KSC Wiedikon DU23-1 - VBC Züri Unterland DU23-1",
+    "start": "2025-09-15T00:00:00",
+    "className": [
+      "open-event"
+    ],
+    "extendedProps": {
+      "group": "Frauen U23 1. Liga",
+      "gym": NaN,
+      "status": "Open",
+      "homeaway": "Away",
+      "wiedikonTeam": "KSC Wiedikon DU23-1"
+    }
+  },
+  {
+    "title": "KSC Wiedikon DU23-1 - VBC Voléro Zürich 1",
+    "start": "2025-09-15T00:00:00",
+    "className": [
+      "open-event"
+    ],
+    "extendedProps": {
+      "group": "Frauen U23 1. Liga",
+      "gym": "Im Birch",
+      "status": "Open",
+      "homeaway": "Away",
+      "wiedikonTeam": "KSC Wiedikon DU23-1"
+    }
+  },
+  {
+    "title": "KSC Wiedikon DU23-1 - Volley Uster A1",
+    "start": "2025-09-15T00:00:00",
+    "className": [
+      "open-event"
+    ],
+    "extendedProps": {
+      "group": "Frauen U23 1. Liga",
+      "gym": NaN,
+      "status": "Open",
+      "homeaway": "Home",
+      "wiedikonTeam": "KSC Wiedikon DU23-1"
+    }
+  },
+  {
+    "title": "KSC Wiedikon DU23-1 - Volley S9 1",
+    "start": "2025-09-15T00:00:00",
+    "className": [
+      "open-event"
+    ],
+    "extendedProps": {
+      "group": "Frauen U23 1. Liga",
+      "gym": "Kaserne",
+      "status": "Open",
+      "homeaway": "Away",
+      "wiedikonTeam": "KSC Wiedikon DU23-1"
+    }
+  },
+  {
+    "title": "KSC Wiedikon DU23-1 - VBC Furttal",
+    "start": "2025-09-15T00:00:00",
+    "className": [
+      "open-event"
+    ],
+    "extendedProps": {
+      "group": "Frauen U23 1. Liga",
+      "gym": NaN,
+      "status": "Open",
+      "homeaway": "Home",
+      "wiedikonTeam": "KSC Wiedikon DU23-1"
+    }
+  },
+  {
+    "title": "KSC Wiedikon DU23-1 - VBC Einsiedeln DU23",
+    "start": "2025-09-15T00:00:00",
+    "className": [
+      "open-event"
+    ],
+    "extendedProps": {
+      "group": "Frauen U23 1. Liga",
+      "gym": NaN,
+      "status": "Open",
+      "homeaway": "Away",
+      "wiedikonTeam": "KSC Wiedikon DU23-1"
+    }
+  },
+  {
+    "title": "KSC Wiedikon DU23-1 - VBC Wetzikon DU23-1",
+    "start": "2025-09-15T00:00:00",
+    "className": [
+      "open-event"
+    ],
+    "extendedProps": {
+      "group": "Frauen U23 1. Liga",
+      "gym": NaN,
+      "status": "Open",
+      "homeaway": "Away",
+      "wiedikonTeam": "KSC Wiedikon DU23-1"
+    }
+  },
+  {
+    "title": "KSC Wiedikon DU23-1 - Volley S9 1",
+    "start": "2025-09-15T00:00:00",
+    "className": [
+      "open-event"
+    ],
+    "extendedProps": {
+      "group": "Frauen U23 1. Liga",
+      "gym": NaN,
+      "status": "Open",
+      "homeaway": "Home",
+      "wiedikonTeam": "KSC Wiedikon DU23-1"
+    }
+  },
+  {
+    "title": "KSC Wiedikon DU23-1 - VBC Wetzikon DU23-1",
+    "start": "2025-09-27T10:30:00",
+    "className": [
+      "open-event",
+      "spielsamstag"
+    ],
+    "extendedProps": {
+      "group": "Frauen U23 1. Liga",
+      "gym": "Kantonsschule Wiedikon (Halle A)",
+      "status": "Open",
+      "homeaway": "Home",
+      "wiedikonTeam": "KSC Wiedikon DU23-1"
+    }
+  },
+  {
+    "title": "KSC Wiedikon HU23-1 - VBC Wetzikon HU23",
+    "start": "2025-09-27T10:30:00",
+    "className": [
+      "open-event",
+      "spielsamstag"
+    ],
+    "extendedProps": {
+      "group": "Männer U23 Gruppe A",
+      "gym": "Kantonsschule Wiedikon C",
+      "status": "Open",
+      "homeaway": "Home",
+      "wiedikonTeam": "KSC Wiedikon HU23-1"
+    }
+  },
+  {
+    "title": "KSC Wiedikon HU23-1 - VBC Rämi",
+    "start": "2025-11-29T16:00:00",
+    "className": [
+      "open-event"
+    ],
+    "extendedProps": {
+      "group": "Männer U23 Gruppe A",
+      "gym": "Kantonsschule Wiedikon C",
+      "status": "Open",
+      "homeaway": "Home",
+      "wiedikonTeam": "KSC Wiedikon HU23-1"
+    }
+  },
+  {
+    "title": "KSC Wiedikon HU23-1 - Wädivolley HU23",
+    "start": "2026-01-31T10:30:00",
+    "className": [
+      "open-event",
+      "spielsamstag"
+    ],
+    "extendedProps": {
+      "group": "Männer U23 Gruppe A",
+      "gym": "Kantonsschule Wiedikon C",
+      "status": "Open",
+      "homeaway": "Home",
+      "wiedikonTeam": "KSC Wiedikon HU23-1"
+    }
+  },
+  {
+    "title": "KSC Wiedikon HU23-1 - VBC Wetzikon HU23",
+    "start": "2026-03-07T13:30:00",
+    "className": [
+      "open-event",
+      "spielsamstag"
+    ],
+    "extendedProps": {
+      "group": "Männer U23 Gruppe A",
+      "gym": "Kantonsschule Wiedikon (Halle B)",
+      "status": "Open",
+      "homeaway": "Home",
+      "wiedikonTeam": "KSC Wiedikon HU23-1"
+    }
+  }
+];
